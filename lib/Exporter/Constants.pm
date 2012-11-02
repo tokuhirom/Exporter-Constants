c9bfe7c067a29bfef9486a65ca0430fa3e1d1e0f
tokuhirom <tokuhirom@gmail.com>
2012-11-02 17:37:34 +0900

    initial import

---
+++ lib/Exporter/Constants.pm
@@ -0,0 +1,39 @@
+package Exporter::Constants;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Exporter::Constants - ...
+
+=head1 SYNOPSIS
+
+  use Exporter::Constants;
+
+=head1 DESCRIPTION
+
+Exporter::Constants is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
