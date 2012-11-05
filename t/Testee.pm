e36acabdd5ccd5effbe94a83cc3c4b2b150a6442
xaicron <xaicron@gmail.com>
2012-11-05 20:36:36 +0900

    support multiple variables

--- t/Testee.pm
+++ t/Testee.pm
@@ -5,11 +5,16 @@ use utf8;
 use parent qw/Exporter/;
 
 our @EXPORT;
+our @EXPORT_OK;
 
 use Exporter::Constants (
     \@EXPORT => {
         TYPE_A => 4649,
         TYPE_B => 5963,
+    },
+    \@EXPORT_OK => {
+        TYPE_C => 1919,
+        TYPE_D => 0721,
     },
 );
 
